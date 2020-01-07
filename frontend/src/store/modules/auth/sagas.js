7a645d3e8722741635b368837048b05a72086e68
Ricardo Lucas <ricardolucas@edu.unifor.br>
2020-01-07 11:51:56 -0300

    [Redux] Criando actions de login

--- frontend/src/store/modules/auth/sagas.js
+++ frontend/src/store/modules/auth/sagas.js
@@ -1,3 +1,24 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, all, call, put } from 'redux-saga/effects';
 
-export default all([]);
+import { signInSuccess } from './actions';
+
+import api from '../../../services/api';
+
+export function* signIn({ payload }) {
+    const { email, password } = payload;
+
+    const response = yield call(api.post, 'session', {
+        email,
+        password
+    });
+
+    const { token, user } = response.data;
+
+    yield put(signInSuccess(token, user));
+
+    
+}
+
+export default all([
+    takeLatest('@auth/SIGN_IN_REQUEST', signIn)
+]);
