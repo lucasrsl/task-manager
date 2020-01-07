63a42c309390a2605195b14a7dcb278fa386a383
Ricardo Lucas <ricardolucas@edu.unifor.br>
2020-01-07 11:38:11 -0300

    [Redux] Configurando redux

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import * as Yup from 'yup';
 
 import { Form, Input } from '@rocketseat/unform';
 
+const schema = Yup.object().shape({
+  email: Yup.string().email('Insira um e-mail válido').required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória')
+});
+
 export default function SignIn() {
   function handleSubmit(data) {
+    console.log(data);
     
   }
 
   return (
     <>
       <h1>Entrar na sua conta</h1>
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="E-mail" />
         <Input name="password" type="password" placeholder="Senha" />
 
--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import * as Yup from 'yup';
 
 import { Form, Input } from '@rocketseat/unform';
 
+const schema = Yup.object().shape({
+  name: Yup.string().required('A nome é obrigatório'),
+  email: Yup.string().email('Insira um e-mail válido').required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória').min(6, 'A senha precisa conter no mínimo 6 caracteres')
+});
+
 export default function SignUp() {
   function handleSubmit(data) {
-
+    console.log(data);
+    
   }
-  
+
   return (
     <>
       <h1>Criar a sua conta</h1>
-      <Form onSubmit={handleSubmit}>
+      <Form schema ={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome" />
         <Input name="email" type="email" placeholder="E-mail" />
         <Input name="password" type="password" placeholder="Senha" />
